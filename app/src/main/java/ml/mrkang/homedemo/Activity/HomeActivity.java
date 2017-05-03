4350ff00cb7f8054f00700b0d7d2888bf2f127bf
kangjinwen <kangjinwen@126.com>
2017-05-03 18:07:28 +0800

    1.去Actionbar android:theme="@style/Theme.AppCompat.Light.NoActionBar"> 2.状态栏透明 setTranslucentStatus(this)

--- app/src/main/java/ml/mrkang/homedemo/Activity/HomeActivity.java
+++ app/src/main/java/ml/mrkang/homedemo/Activity/HomeActivity.java
@@ -1,8 +1,16 @@
 package ml.mrkang.homedemo.Activity;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
 import ml.mrkang.homedemo.R;
 import ml.mrkang.homedemo.View.GlideImageLoader;
 import com.youth.banner.Banner;
@@ -23,6 +31,9 @@ public class HomeActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setTranslucentStatus(this);
+
         for(int res:images){
             phos.add(res);
         }
@@ -30,11 +41,10 @@ public class HomeActivity extends AppCompatActivity  {
         tits.add("2");
         tits.add("3");
         tits.add("4");
-
         setContentView(R.layout.activity_home);
         Banner banner = (Banner) findViewById(R.id.banner);
         //设置banner样式
-        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE);
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR);
         //设置图片加载器
         banner.setImageLoader(new GlideImageLoader());
         //设置图片集合
@@ -62,7 +72,6 @@ public class HomeActivity extends AppCompatActivity  {
                         startActivity(intent);
                         break;
                 }
-
             }
         });
         //设置指示器位置（当banner模式中有指示器时）
@@ -70,5 +79,15 @@ public class HomeActivity extends AppCompatActivity  {
         //banner设置方法全部调用完毕时最后调用
         banner.start();
     }
-
+    public static void setTranslucentStatus(Activity activity){
+        Window window = activity.getWindow();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
 }
